c5f87f937a47bb73766745768d97b9d027820c81
arminask <arminas@ver.lt>
2024-08-17 13:36:07 +0300

    Fix WDF_VIOLATION issue

--- driver/vhidmini.c
+++ driver/vhidmini.c
@@ -527,7 +527,8 @@ Return Value:
 
     deviceContext = GetDeviceContext(device);
     deviceContext->Device       = device;
-    deviceContext->DeviceData   = 0;
+    deviceContext->DeviceData = 0;
+    deviceContext->OnClose = FALSE;
 
     hidAttributes = &deviceContext->HidDeviceAttributes;
     RtlZeroMemory(hidAttributes, sizeof(HID_DEVICE_ATTRIBUTES));
@@ -738,6 +739,7 @@ NTSTATUS
                 OnInterruptIsr,
                 NULL);
 
+            interruptConfig.ReportInactiveOnPowerDown = TRUE;
             interruptConfig.PassiveHandling = TRUE;
             interruptConfig.InterruptTranslated = WdfCmResourceListGetDescriptor(
                 FxResourcesTranslated,
@@ -2050,6 +2052,8 @@ OnInterruptIsr(
 
     device = WdfInterruptGetDevice(FxInterrupt);
     pDevice = GetDeviceContext(device);
+    if (pDevice->OnClose)
+        return TRUE;
 
     //
     // Notify the app that an interrupt has occurred.
@@ -2208,7 +2212,7 @@ SpbDeviceClose(
     _In_  PDEVICE_CONTEXT  pDevice
 )
 {
-    WdfInterruptDisable(pDevice->Interrupt);
+    pDevice->OnClose = TRUE;
 
     WdfIoTargetClose(pDevice->SpbController);
 }
